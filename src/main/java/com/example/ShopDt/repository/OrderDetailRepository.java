8d68aa1d73df4d51f05d13947f2c8c40501048a3
datgiaodau <phamtuandat50@gmail.com>
2026-04-26 01:05:26 +0700

    update revuene chart admin

--- src/main/java/com/example/ShopDt/repository/OrderDetailRepository.java
+++ src/main/java/com/example/ShopDt/repository/OrderDetailRepository.java
@@ -3,12 +3,21 @@ package com.example.ShopDt.repository;
 import com.example.ShopDt.entity.OrderDetail;
 import com.example.ShopDt.entity.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import org.springframework.data.domain.Pageable;
 import java.util.List;
 
 @Repository
 public interface OrderDetailRepository extends JpaRepository<OrderDetail, Long> {
     List<OrderDetail> findByProduct(Product product);
     boolean existsByProduct(Product product);
+
+    // Lấy top 10 sản phẩm bán chạy nhất
+    @Query("SELECT p.name, SUM(od.quantity), SUM(od.price), p.image " +
+            "FROM OrderDetail od JOIN od.product p " +
+            "GROUP BY p.id, p.name, p.image " +
+            "ORDER BY SUM(od.quantity) DESC")
+    List<Object[]> getTopSellingProducts(Pageable pageable);
 }
