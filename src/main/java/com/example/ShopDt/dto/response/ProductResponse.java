621681bfd83796d6b2d09da50ace56f15654e358
datgiaodau <phamtuandat50@gmail.com>
2026-04-26 04:39:50 +0700

    update edit status category product admin

--- src/main/java/com/example/ShopDt/dto/response/ProductResponse.java
+++ src/main/java/com/example/ShopDt/dto/response/ProductResponse.java
@@ -4,6 +4,8 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.util.List;
+
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
@@ -15,5 +17,5 @@ public class ProductResponse {
     private String image;
     private String description;
     private int status;
-
+    private List<CategoryResponse> categories;
 }
--- src/main/java/com/example/ShopDt/mapper/product/ProductMapper.java
+++ src/main/java/com/example/ShopDt/mapper/product/ProductMapper.java
@@ -1,10 +1,17 @@
 package com.example.ShopDt.mapper.product;
 
+import com.example.ShopDt.dto.response.CategoryResponse;
 import com.example.ShopDt.entity.Product;
 import com.example.ShopDt.dto.request.ProductRequest;
 import com.example.ShopDt.dto.response.ProductResponse;
+import com.example.ShopDt.entity.ProductCategory;
 import org.mapstruct.Mapper;
 import org.mapstruct.MappingTarget;
+import org.springframework.web.bind.annotation.Mapping;
+
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 @Mapper(componentModel = "spring")
 public interface ProductMapper {
